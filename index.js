adfe74c936a97578c462eed04cb658afff1ee3cc
[sharoun <[madoyasharoun@gmail.com]>
2023-03-13 01:03:25 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function introduction (name){
+    return 'Hi, my name is #{name}.' 
+
+}
+function introductionWithLanguage (name, language){
+    return 'Hi, my name is #{name} and I am learning to program in #{language}.'
+    return 'Hi, my name is Aki and I am learning to program in Ember.js.'
+}
+function introductionWithLanguageOptional (name, language= "Javascript"){
+    return 'Hi, my name is Gracie and I am learning to program in Python.'
+}
+function introductionWithLanguageOptional(name, language){
+    return 'Hi, my name is Gracie and I am learning to program in JavaScript.'
+}
